27f51fa2a39aa2592f45a72339af4aac4084e9cf
Vivekanand P V <vivekanandpv@outlook.com>
2022-09-13 18:40:07 +0530

    done

--- Program.cs
+++ Program.cs
@@ -1,32 +1,13 @@
 ﻿namespace CSharp.Methods.Study {
     internal class Program {
         static void Main(string[] args) {
-            Sum(8, 9);  //  Sum(int,int) is proximal
-            Sum(5.0, 6);    //  Sum(double,int)
+            Sum(14);    //  x -> 14 and y -> 0 (default)
+            Sum(12, 15);    //  x -> 12 and y -> 15
+            Sum(y: 8);  //  x -> 0 and y -> 8 (using named parameter)
         }
 
-        //  So long as parameter type list is different methods can be overloaded
-        //  Overloaded methods can have different return types
-        //  Rule of proximal types
-        //  Beware of call-ambiguity
-
-        //  Sum(int,int)
-        static int Sum(int x, int y) {
-            return x + y;
-        }
-
-        //  Sum(double,double)
-        static double Sum(double x, double y) {
-            return x + y;
-        }
-
-        //  Sum(double,int)
-        static double Sum(double x, int y) {
-            return x + y;
-        }
-
-        //  Sum(int,double)
-        static double Sum(int x, double y) {
+        //  The optional parameters must come after the required parameters
+        static int Sum(int x = 0, int y = 0) {
             return x + y;
         }
     }
