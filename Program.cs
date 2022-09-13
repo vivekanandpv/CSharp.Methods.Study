746793378e1bb3a3db86c08b8425b783fae7ace0
Vivekanand P V <vivekanandpv@outlook.com>
2022-09-13 19:38:53 +0530

    done

--- Program.cs
+++ Program.cs
@@ -1,7 +1,23 @@
 ﻿namespace CSharp.Methods.Study {
     internal class Program {
         static void Main(string[] args) {
-            Console.WriteLine("Hello, World!");
+            //  You may need to import the source of PrintString
+            "Hello, world".PrintString();
+        }
+
+        
+    }
+
+    //  Extension methods lets you add additional methods
+    //  without modifying the existing type
+
+    //  An extension method is a static method in a static class
+    //  The first argument should be this <type>
+
+    //  LINQ, EF, etc use extension methods a lot
+    static class StringExtensions {
+        public static void PrintString(this string s) {
+            Console.WriteLine($"Print String: {s}");
         }
     }
 }
