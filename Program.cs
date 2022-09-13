afbca74e3ddce91a09db2c6f17f8819db69f8cfc
Vivekanand P V <vivekanandpv@outlook.com>
2022-09-13 19:27:53 +0530

    done

--- Program.cs
+++ Program.cs
@@ -1,22 +1,31 @@
 ﻿namespace CSharp.Methods.Study {
     internal class Program {
         static void Main(string[] args) {
-            //  classical approach
-            int a;
-            double b;
-            Foo(out a, out b, 7);   //  out parameters should be called with out
+            var complexNumber = new ComplexNumber { Real = 9, Imaginary = 7};
 
-            //   Since C# 6.0
-            Foo(out int i, out double d, 89);    // i and d are in scope from this point
+            Foo(complexNumber);    //  
         }
 
-        //  In C#, it is possible to return multiple values using out parameters
-        //  out parameters cannot have default values
-        //  out cannot be used with keywords: params or ref or in
-        static void Foo(out int x, out double y, int z) {
-            //  All out parameters must be assigned before return
-            x = 90;
-            y = 3.1415926535 * z;
+        //  Introduced in C# 7.2, in parameters are readonly-references
+        //  Useful dealing with heavy-weight structs
+        //  in parameter is a reference, so the copy is not made
+        //  and in parameter is readonly
+        static void Foo(in ComplexNumber cn) {
+            //  in parameters cannot be assigned
+            //  but mutation is allowed
+
+            //  cn = new ComplexNumber { Real = 3, Imaginary = 5 }; //  illegal
+
+            Console.WriteLine(cn);
+        }
+    }
+
+    struct ComplexNumber {
+        public double Real { get; set; }
+        public double Imaginary { get; set; }
+
+        public override string ToString() {
+            return $"Real = {Real}; Imaginary = {Imaginary}";
         }
     }
 }
