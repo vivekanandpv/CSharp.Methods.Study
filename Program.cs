edd9e3d68b6ec033908f39c2ff60af033e66b58e
Vivekanand P V <vivekanandpv@outlook.com>
2022-09-13 19:03:56 +0530

    done

--- Program.cs
+++ Program.cs
@@ -1,28 +1,24 @@
 ﻿namespace CSharp.Methods.Study {
     internal class Program {
         static void Main(string[] args) {
-            //  C# supports the pass by reference mode
-            int i = 9;
-            Foo(ref i); //  the caller should pass the argument with ref
-            Console.WriteLine(i);   //  10
-
-            int[] scores = { 7, 8, 9 };
-            Foo(ref scores);
-            Console.WriteLine(scores == null);    //  True as scores is null now
+            Foo(9, 8.65, 1, 2, 3, 4, 5);    //  1, 2, 3, 4, 5 are passed as varargs
         }
 
-        //  The method must declare the parameter as ref
-        static void Foo(ref int x) {
-            //  x is the same reference to the argument
-            //  below action changes the argument!
-            ++x;
-        }
+        //  C# supports variadic methods
 
-        static void Foo(ref int[] ar) {
-            //  ar is the same reference to the argument passed
-            //  below action changes the argument!
+        //  Rules:
+        //  a method can take only one params
+        //  params must be at the end
+        //  params type should be an array
+        //  params is optional
+        //  default value for the parameter array is not allowed
+        static void Foo(int x, double y, params int[] z) {
+            Console.WriteLine($"int x = {x}");
+            Console.WriteLine($"double y = {y}");
 
-            ar = null;
+            foreach (var el in z) {
+                Console.WriteLine($"Element in z -> {el}");
+            }
         }
     }
 }
