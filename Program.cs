cf7e79a0f65c6d6045c56263608737861fc54096
Vivekanand P V <vivekanandpv@outlook.com>
2022-09-13 18:33:50 +0530

    done

--- Program.cs
+++ Program.cs
@@ -1,13 +1,32 @@
 ﻿namespace CSharp.Methods.Study {
     internal class Program {
         static void Main(string[] args) {
-            Console.WriteLine("Hello, World!");
+            Sum(8, 9);  //  Sum(int,int) is proximal
+            Sum(5.0, 6);    //  Sum(double,int)
         }
 
-        //  Method signature: name(parameter type list)
+        //  So long as parameter type list is different methods can be overloaded
+        //  Overloaded methods can have different return types
+        //  Rule of proximal types
+        //  Beware of call-ambiguity
+
         //  Sum(int,int)
-        //  does not include return type and parameter names
         static int Sum(int x, int y) {
+            return x + y;
+        }
+
+        //  Sum(double,double)
+        static double Sum(double x, double y) {
+            return x + y;
+        }
+
+        //  Sum(double,int)
+        static double Sum(double x, int y) {
+            return x + y;
+        }
+
+        //  Sum(int,double)
+        static double Sum(int x, double y) {
             return x + y;
         }
     }
