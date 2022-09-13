323b8a9fa9ee4cf3626c2be6237f668f59ebf330
Vivekanand P V <vivekanandpv@outlook.com>
2022-09-13 18:29:00 +0530

    done

--- Program.cs
+++ Program.cs
@@ -3,5 +3,12 @@
         static void Main(string[] args) {
             Console.WriteLine("Hello, World!");
         }
+
+        //  Method signature: name(parameter type list)
+        //  Sum(int,int)
+        //  does not include return type and parameter names
+        static int Sum(int x, int y) {
+            return x + y;
+        }
     }
 }
