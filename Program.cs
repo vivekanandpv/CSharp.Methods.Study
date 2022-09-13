4664286cd12f60b72e052df16b1ef9265057f0f5
Vivekanand P V <vivekanandpv@outlook.com>
2022-09-13 18:56:49 +0530

    done

--- Program.cs
+++ Program.cs
@@ -1,26 +1,28 @@
 ﻿namespace CSharp.Methods.Study {
     internal class Program {
         static void Main(string[] args) {
-            //  Parameters are passed by value by default
+            //  C# supports the pass by reference mode
             int i = 9;
-            Foo(i);
-            Console.WriteLine(i);   //  still 9
+            Foo(ref i); //  the caller should pass the argument with ref
+            Console.WriteLine(i);   //  10
 
             int[] scores = { 7, 8, 9 };
-            Foo(scores);
-            Console.WriteLine($"{scores[0]}, {scores[1]}, {scores[2]}");    //  scores is intact
+            Foo(ref scores);
+            Console.WriteLine(scores == null);    //  True as scores is null now
         }
 
-        static void Foo(int x) {
-            ++x;    //  Foo gets its own copy of x
+        //  The method must declare the parameter as ref
+        static void Foo(ref int x) {
+            //  x is the same reference to the argument
+            //  below action changes the argument!
+            ++x;
         }
 
-        static void Foo(int[] ar) {
-            //  ar is a reference copy of the argument passed
-            //  If ar changes to a new value (null in this case),
-            //  it doesn't affect the argument's binding with the object
+        static void Foo(ref int[] ar) {
+            //  ar is the same reference to the argument passed
+            //  below action changes the argument!
 
-            ar = null;  
+            ar = null;
         }
     }
 }
